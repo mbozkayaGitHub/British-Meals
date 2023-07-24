b91a32b97ad12f8277cdbe6dce8ad71281cad357
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-24 14:46:07 +0300

    htmlAppjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>react start page</h1>
+      <h1 className="header">
+        British Meals
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/2560px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png" />
+      </h1>
+
+      <div className="container">
+        <div className="card">
+          <div className="image">
+          <img src="" alt="picture" />
+
+          </div>
+          <p>...</p>
+        </div>
+
+        <button>Click for more food</button>
+      </div>
     </div>
   );
 }

670fcc02307e3681fceb9794e3a3fc7b00227f1d
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-24 15:18:55 +0300

    App finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([])
+  const [visible, setVisible] = useState(3)
+
+  function showMoreFood () {
+    setVisible(item=> item+= 3)
+  }
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=british")
+      .then((res) => res.json())
+      .then((data) => setItems(data.meals));
+  }, []);
+
+
+
   return (
     <div className="App">
       <h1 className="header">
@@ -9,15 +25,18 @@ function App() {
       </h1>
 
       <div className="container">
+   {items.slice(0,visible).map(item => (
         <div className="card">
           <div className="image">
-          <img src="" alt="" />
-
+            <img src={item.strMealThumb} alt="" />
           </div>
-          <p>...</p>
+          <p>{item.strMeal}</p>
         </div>
+))}
 
-        <button>Click for more food</button>
+
+
+        <button onClick={showMoreFood}>Click for more food</button>
       </div>
     </div>
   );
